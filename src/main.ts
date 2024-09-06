6dad0c72e4818e190a28d4cfaea4386e01123954
jianghua1 <719584530@qq.com>
2024-09-06 20:19:11 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import HelloWorldVue from "./components/HelloWorld.vue";
+import TheWelcomeVue from "./components/TheWelcome.vue";
+import WelcomeItemVue from "./components/WelcomeItem.vue";
+
+export { HelloWorldVue, TheWelcomeVue, WelcomeItemVue };
---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+
+import pkg from "./package.json";
+import dts from "vite-plugin-dts";
+
+const pkgName = pkg.name;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), dts({ rollupTypes: true })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  build: {
+    sourcemap: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: pkgName,
+      // the proper extensions will be added
+      formats: ["es", "umd"],
+      fileName: (format) => `${pkgName}.${format}.ts`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ["vue"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});

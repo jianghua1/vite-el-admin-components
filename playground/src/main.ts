986e32e5b8e886430263b451a0868ca489b982b9
jianghua1 <719584530@qq.com>
2024-09-08 01:21:00 +0800

    5.1.3.2 对比两种不同的打包方案（是否要外置核心库？）

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import ElementPlus from 'element-plus'
+import ElementPlus from 'element-plus'
 
 import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
+// import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'el-admin-components/dist/style.css'
 const app = createApp(App)
+app.use(ElementPlus)
 app.mount('#app')
